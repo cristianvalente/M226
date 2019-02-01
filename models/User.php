b8485616b67540c02dfd046f67e9b8d022c6ce39
cristianvalente <32476836+cristianvalente@users.noreply.github.com>
2019-02-01 21:17:07 +0530

    add findByUsername

--- models/User.php
+++ models/User.php
@@ -315,7 +315,7 @@ class User extends ActiveRecord implements IdentityInterface
      * @return \yii\db\ActiveQuery
      */
    
-public static function findByUsername($username)
+    public static function findByUsername($username)
     {
         return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
     }
