773deebdcfa13db0cf5618a966f8ab7391925360
cristianvalente <32476836+cristianvalente@users.noreply.github.com>
2019-02-01 15:22:24 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=netjobs',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=aeschlic.mysql.db.internal;dbname=aeschlic_valen',
+    'username' => 'aeschlic_valen',
+    'password' => 'm226.2019!',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
